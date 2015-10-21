eb42c54d4959e9b1f615cfd89056fefc48fc3303
Curtis Maloney <curtis@tinbrain.net>
2015-10-21 16:37:00 +1100

    Support change in templates in django 1.8

--- email_template/util.py
+++ email_template/util.py
@@ -1,7 +1,11 @@
+from django import VERSION
 from django.template.loader_tags import BlockNode
 
 
 def get_node(template, name):
+    if VERSION > (1.8):
+        # Pluggable template engines beget template wrapper classes
+        template = template.template
     for node in template.nodelist.get_nodes_by_type(BlockNode):
         if node.name == name:
             return node
